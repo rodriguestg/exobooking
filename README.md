e12c29262e0ad9fd95daaafaf8fea8060964b3aa
Gabriel Rodrigues <grs.ibr@gmail.com>
2026-02-20 16:01:23 -0300

    feat: initial plugin structure and docker environment

---
+++ README.md
@@ -0,0 +1,24 @@
+﻿# ExoBooking Core
+
+Plugin WordPress para gerenciamento de reservas com protecao anti-overbooking.
+
+## Como rodar
+
+1. Clone o repositorio
+2. Execute: docker-compose up -d
+3. Acesse http://localhost:8000 e instale o WordPress
+4. Va em Plugins e ative o ExoBooking Core
+
+## Teste de Concorrencia
+
+Dispare 5 requisicoes simultaneas para:
+POST http://localhost:8000/wp-json/exobooking/v1/bookings
+
+Esperado: 3 aprovadas, 2 bloqueadas com erro de vagas esgotadas.
+
+## Stack
+
+- PHP 7.4+
+- WordPress 6.x
+- MySQL 8.0 (InnoDB)
+- Docker + docker-compose
---
+++ wp-content/plugins/exobooking-core/exobooking-core.php
@@ -0,0 +1,26 @@
+﻿<?php
+/**
+ * Plugin Name: ExoBooking Core
+ * Description: Sistema anti-overbooking para reservas de passeios
+ * Version: 1.0.0
+ * Author: Gabriel Rodrigues
+ * Requires PHP: 7.4
+ */
+
+if (!defined('ABSPATH')) exit;
+
+define('EXOBOOKING_VERSION', '1.0.0');
+define('EXOBOOKING_PATH', plugin_dir_path(__FILE__));
+define('EXOBOOKING_URL', plugin_dir_url(__FILE__));
+
+// Executado uma vez ao ativar o plugin
+register_activation_hook(__FILE__, 'exobooking_activate');
+function exobooking_activate() {
+    flush_rewrite_rules();
+}
+
+// Executado ao desativar
+register_deactivation_hook(__FILE__, 'exobooking_deactivate');
+function exobooking_deactivate() {
+    flush_rewrite_rules();
+}
